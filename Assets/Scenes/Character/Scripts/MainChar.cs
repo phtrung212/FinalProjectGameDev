4fd2951c6283bc264c1085c2a3f06ef0a8ee2395
huutuanhcmus <huutuan.hcmus@gmail.com>
2018-12-28 08:00:07 +0700

    Di chuyen co ban Player, Boss

---
+++ Assets/Scenes/Character/Scripts/MainChar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainChar : MonoBehaviour {
+    public float speed = 5f;
+    public float jumpSpeed = 10f;
+    private float moment = 0f;
+    private Rigidbody2D rigidBody;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+    private Animator playerAnimation;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        moment = Input.GetAxis("Horizontal");
+        if (moment > 0f)
+        {
+            rigidBody.velocity = new Vector2(moment * speed, rigidBody.velocity.y);
+            transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+        else if(moment < 0)
+        {
+            rigidBody.velocity = new Vector2(moment * speed, rigidBody.velocity.y);
+            transform.localScale = new Vector2(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+        }
+        playerAnimation.SetFloat("Speed", Mathf.Abs(rigidBody.velocity.x));
+        playerAnimation.SetBool("onGround", isTouchingGround);
+    }
+}
---
+++ Assets/Scenes/Quai/AutoAttack.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoAttack : MonoBehaviour {
+
+    public GameObject player;
+    public float speed = 5f;
+    public float jumpSpeed = 10f;
+    private Rigidbody2D rigidBody;
+    private Animator playerAnimation;
+    bool flag;
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
+        flag = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (flag == true)
+        {
+            Debug.Log("ccccc");
+            Debug.Log(Mathf.Abs(player.transform.position.x - transform.position.x));
+            if (Mathf.Abs(player.transform.position.x - transform.position.x) < 3.5)
+            {
+                playerAnimation.SetBool("Attack", true);
+            }
+            else
+            {
+                Debug.Log("ccccc11");
+                if (player.transform.position.x < transform.position.x)
+                {
+                    Debug.Log(flag);
+                    rigidBody.velocity = new Vector2(-Mathf.Abs(speed), rigidBody.velocity.y);
+                    transform.localScale = new Vector2(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
+                }
+                else if (player.transform.position.x > transform.position.x)
+                {
+                    rigidBody.velocity = new Vector2(Mathf.Abs(speed), rigidBody.velocity.y);
+                    transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+                }
+                playerAnimation.SetFloat("Speed", 0.2f);
+                playerAnimation.SetBool("Attack", false);
+            }
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+            playerAnimation.SetFloat("Speed", 0f);
+        }
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("aaaaa");
+            flag = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("bbbbbb");
+            flag = false;
+        }
+    }
+}
