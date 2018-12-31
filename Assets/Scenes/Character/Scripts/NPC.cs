c9bce274a5898a4b40411a898b010253943dc35f
huutuanhcmus <huutuan.hcmus@gmail.com>
2019-01-01 06:45:57 +0700

    updated scenes 12 map

--- Assets/Scenes/Character/Scripts/NPC.cs
+++ Assets/Scenes/Character/Scripts/NPC.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NPC : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int level;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log(collision.gameObject.tag);
+        //if (collision.gameObject.tag == "Player") {
+        //Debug.Log("1111111");
+        //{
+        Debug.Log("2222222222");
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+        //}
+
+        //}
+    } 
 }
